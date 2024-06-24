ba73a75541f0389990d408a7688b59fc92399e60
Sasindu <sasindusavi@gmail.com>
2024-06-24 22:58:40 +0530

    tictactoe from cpp

---
+++ tictactoe.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+// Function to print the Tic Tac Toe board
+void printBoard(char board[3][3]) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cout << board[i][j];
+            if (j < 2) cout << " | ";
+        }
+        cout << endl;
+        if (i < 2) cout << "--|---|--" << endl;
+    }
+}
+
+// Function to check if there is a winner
+char checkWinner(char board[3][3]) {
+    // Check rows and columns
+    for (int i = 0; i < 3; ++i) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) return board[i][0];
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) return board[0][i];
+    }
+    // Check diagonals
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) return board[0][0];
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) return board[0][2];
+    // No winner
+    return ' ';
+}
+
+// Function to check if the board is full
+bool isBoardFull(char board[3][3]) {
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            if (board[i][j] == ' ') return false;
+    return true;
+}
+
+int main() {
+    char board[3][3] = { {' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '} };
+    char currentPlayer = 'X';
+    char winner = ' ';
+
+    while (winner == ' ' && !isBoardFull(board)) {
+        printBoard(board);
+        int row, col;
+        cout << "Player " << currentPlayer << ", enter your move (row and column): ";
+        cin >> row >> col;
+
+        if (row >= 1 && row <= 3 && col >= 1 && col <= 3 && board[row - 1][col - 1] == ' ') {
+            board[row - 1][col - 1] = currentPlayer;
+            winner = checkWinner(board);
+            if (winner == ' ' && isBoardFull(board)) break;
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        } else {
+            cout << "Invalid move. Try again." << endl;
+        }
+    }
+
+    printBoard(board);
+    if (winner != ' ') {
+        cout << "Player " << winner << " wins!" << endl;
+    } else {
+        cout << "It's a tie!" << endl;
+    }
+
+    return 0;
+}
